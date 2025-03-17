be85c92447aedb1fcbe5f83df7afae56d1682b5f
igrisdev <johanalvarezz237@gmail.com>
2025-03-16 20:53:43 -0500

    add Slider component with Swiper integration for image carousel

--- src/sections/Slider.jsx
+++ src/sections/Slider.jsx
@@ -1,3 +1,32 @@
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/autoplay'
+
+import { Pagination, Autoplay } from 'swiper/modules'
+
 export const Slider = () => {
-  return <div>Slider</div>
+  return (
+    <section className='bg-blue-100 h-screen overflow-hidden'>
+      <Swiper
+        pagination={{
+          dynamicBullets: true,
+        }}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        loop={true}
+        modules={[Pagination, Autoplay]}
+        className='mySwiper h-full'
+      >
+        <SwiperSlide>
+          <img src='/main_image.jpg' alt='Slider 1' />
+        </SwiperSlide>
+        <SwiperSlide className='object-cover'>
+          <img src='/slider_2.jpg' alt='Slider 2' />
+        </SwiperSlide>
+      </Swiper>
+    </section>
+  )
 }
