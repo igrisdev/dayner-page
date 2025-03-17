66d6622d890139cc5b8777673af61705420a2738
igrisdev <johanalvarezz237@gmail.com>
2025-03-16 20:54:13 -0500

    add MainImage section with background and ButtonBuy integration

--- src/sections/MainImage.jsx
+++ src/sections/MainImage.jsx
@@ -1,3 +1,23 @@
+import { ButtonBuy } from '../components/ButtonBuy'
+
 export const MainImage = () => {
-  return <div>MainImage</div>
+  return (
+    <section
+      style={{
+        backgroundImage: 'url(/main_image.jpg)',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+      className='aspect-video overflow-hidden flex flex-col items-center justify-center gap-6'
+    >
+      <div className='text-center max-w-5xl flex flex-col gap-2'>
+        <span className='text-white/80 text-xl'>Lorem Ipsum</span>
+        <h2 className='text-white text-6xl font-semibold'>
+          Lorem ipsum dolor sit amet
+        </h2>
+      </div>
+
+      <ButtonBuy />
+    </section>
+  )
 }
