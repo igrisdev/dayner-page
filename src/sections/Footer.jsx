5e01a49dfa78b654562201aef5aaad879827cf7d
igrisdev <johanalvarezz237@gmail.com>
2025-03-16 21:15:19 -0500

    add Footer component with social media links and contact form

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,3 +1,47 @@
+import { Twitter } from 'lucide-react'
+import { Instagram } from 'lucide-react'
+
 export const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer className='bg-blue-950 py-10 text-white'>
+      <section className='max-w-7xl mx-auto'>
+        <div className='grid grid-cols-2 mb-10 gap-4'>
+          <div>
+            <h2 className='text-2xl font-semibold mb-2'>Sobre Nosotros</h2>
+            <p className='text-white/80'>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo,
+              sapiente? Quidem asperiores error temporibus saepe, ipsum
+              quibusdam iste nemo quam eos vero qui et sequi ab vel deleniti
+              atque voluptatum.
+            </p>
+          </div>
+
+          <div className='grid gap-3'>
+            <h2 className='text-2xl font-semibold'>Contactar</h2>
+
+            <input
+              type='email'
+              placeholder='ejemplo@ejemplo.com'
+              className='w-full rounded-md border-2 border-white/80 p-2'
+            />
+
+            <div className='flex gap-6'>
+              <a href='/'>
+                <Instagram size={42} />
+              </a>
+              <a href='/'>
+                <Twitter size={42} />
+              </a>
+            </div>
+          </div>
+        </div>
+
+        <div className='grid place-items-center'>
+          <a href='/' className='uppercase text-2xl font-black'>
+            scent
+          </a>
+        </div>
+      </section>
+    </footer>
+  )
 }
