3d07d824803c30d896c7258c4e06fb96d82a7e5a
igrisdev <johanalvarezz237@gmail.com>
2025-03-17 20:40:42 -0500

    update Header component to use new logo file and adjust positioning

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -1,13 +1,13 @@
 export const Header = () => {
   return (
-    <div className='sticky z-90 top-6'>
+    <div className='absolute z-50 top-4 left-4'>
       <div className='grid place-items-center'>
         <a
           href='/'
           className='uppercase text-4xl font-black text-white flex items-center justify-center gap-2'
         >
           <img
-            src='/logo.svg'
+            src='/logo_2.svg'
             alt='Logo Scent'
             className='w-10 h-10 md:w-12 md:h-12'
           />
