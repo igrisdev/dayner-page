4f62daf720b01c789b22b8c722f25a7b35f35cb2
igrisdev <johanalvarezz237@gmail.com>
2025-03-17 19:56:48 -0500

    update MainImage component for responsive typography adjustments

--- src/sections/MainImage.jsx
+++ src/sections/MainImage.jsx
@@ -12,7 +12,7 @@ export const MainImage = () => {
     >
       <div className='text-center max-w-5xl flex flex-col gap-2'>
         <span className='text-white/80 text-xl'>Lorem Ipsum</span>
-        <h2 className='text-white text-6xl font-semibold'>
+        <h2 className='lg:text-6xl md:text-4xl text-2xl text-white  font-semibold'>
           Lorem ipsum dolor sit amet
         </h2>
       </div>
