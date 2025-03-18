88199befc6f55e6985bdc570fc8a517b30056f9c
igrisdev <johanalvarezz237@gmail.com>
2025-03-17 19:56:45 -0500

    update Info section styling for improved responsiveness and readability

--- src/sections/Info.jsx
+++ src/sections/Info.jsx
@@ -3,9 +3,9 @@ import { ButtonBuy } from '../components/ButtonBuy'
 export const Info = () => {
   return (
     <section className='bg-blue-950'>
-      <div className='container mx-auto flex flex-col items-center justify-center h-[35vh] gap-6'>
-        <div className='flex flex-col items-center gap-4 text-center max-w-5xl'>
-          <h2 className='text-3xl font-semibold text-white'>
+      <div className='container mx-auto flex flex-col items-center justify-center sm:h-[35vh] gap-6 py-6 md:py-0'>
+        <div className='flex flex-col items-center gap-4 text-center max-w-5xl px-4 md:px-0'>
+          <h2 className='lg:text-3xl md:text-2xl text-xl font-semibold text-white'>
             A subtitle lorem ipsum dolor sit amet, consectetur adipisicing
           </h2>
 
